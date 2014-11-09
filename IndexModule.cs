6daa2f71acc281fb4ed8bf4d36185f75e586d6a8
Michal Dymel <michal.dymel@wayn.com>
2014-11-09 12:11:08 +0100

    init

---
+++ IndexModule.cs
@@ -0,0 +1,33 @@
+﻿using Nancy.Json;
+using Nancy.ModelBinding;
+
+namespace ModelListBinding
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+
+            Post["/new"] = _ =>
+            {
+                var model = this.Bind<Model>();
+                return new JavaScriptSerializer().Serialize(model);
+            };
+        }
+    }
+
+    public class Model
+    {
+        public string Text1 { get; set; }
+        public string Text2 { get; set; }
+        public string[] List { get; set; }
+        
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Example project showing how to bind post to a list of strings in NancyFX. 
+http://stackoverflow.com/questions/26672023/how-to-code-a-nancy-bind-with-a-mix-of-array-and-non-array-post-variables
